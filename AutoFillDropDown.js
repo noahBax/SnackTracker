66bbd3b8f08234d5117b7915a7a0a86f63723465
Noah Baxley <nbaxley@hawk.iit.edu>
2021-08-11 11:09:07 -0500

    Initial commit

---
+++ AutoFillDropDown.js
@@ -0,0 +1,10 @@
+class AutoFillDropDown extends HTMLElement {
+    constructor() {
+        super();
+
+        // Create a shadow root
+        this.attachShadow({mode: 'open'}); // sets and returns this.shadowRoot
+
+        
+    }
+}
